d214680b495db3b634d4e6b8a6e9467986bd3ff0
Tukeque <TukequeMail@gmail.com>
2020-07-12 13:35:19 +0200

    start branch?? idk how github work lole

---
+++ Tukxel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tukxel
+{
+    class Tukxel
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+            Console.ReadKey();
+        }
+    }
+}
