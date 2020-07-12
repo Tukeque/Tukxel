0ab368bdb31e316f06394fa7ec7fffcd7ff2cda3
Tukeque <TukequeMail@gmail.com>
2020-07-12 13:41:54 +0200

    um added like a text file ya and a renderer.cs cass ya

---
+++ ReadMeIfYouWant.txt
@@ -0,0 +1,3 @@
+﻿helo i make voxel engine i think maybe i also use opentk opentk cool sometimes i guess yeah ok
+
+thank you for you're attention
---
+++ Renderer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tukxel
+{
+    class Renderer
+    {
+    }
+}
